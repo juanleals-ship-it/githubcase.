87592ef45b1e54804336fdbc08d19e263ace78de
juanleals-ship-it <juan.leals@uniagustiniana.edu.co>
2026-08-27 15:13:13 +0000

    Script

---
+++ script.js
@@ -0,0 +1,15 @@
+var texto = "tree"
+var numero = 345;
+var boleano = true;
+var lista =["dsa", "dsad"]
+var obejeto = {"nombre": "juan"}
+function mifuncion(){
+    var texto;
+    if (parametro > texto){
+        texto = true 
+    
+    } else 
+        texto = false 
+        return texto 
+}
+console.log(mifuncion(7))
